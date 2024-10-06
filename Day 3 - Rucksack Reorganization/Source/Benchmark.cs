7d1464ae9aacd9e5639e6c8a4c469dac82525ff3
Philipp Raschka <162504898+Piwimau@users.noreply.github.com>
2024-10-06 18:02:32 +0200

    Add implementation for day 3.

---
+++ Day 3 - Rucksack Reorganization/Source/Benchmark.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+
+namespace RucksackReorganization.Source;
+
+/// <summary>
+/// Represents a <see cref="Benchmark"/> for the <see cref="RucksackReorganization"/> puzzle.
+/// </summary>
+[MemoryDiagnoser]
+public class Benchmark {
+
+    /// <summary>Runs a benchmark for the <see cref="RucksackReorganization"/> puzzle.</summary>
+    [Benchmark]
+    [SuppressMessage(
+        "Performance",
+        "CA1822:Mark members as static",
+        Justification = "Benchmarking static methods is not supported."
+    )]
+    public void Run() => RucksackReorganization.Solve(TextWriter.Null);
+
+}
